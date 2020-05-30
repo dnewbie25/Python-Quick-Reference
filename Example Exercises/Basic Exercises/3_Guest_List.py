443508023a4b995af25ee9b39b2df5c73b94e3f0
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2020-05-29 22:59:41 -0500

    Update 3_Guest_List.py

--- Example Exercises/Basic Exercises/3_Guest_List.py
+++ Example Exercises/Basic Exercises/3_Guest_List.py
@@ -1,6 +1,17 @@
 print("Add the people you want to invite. Remember they must have first name and last name.")
 
-def guests(add_guest):
-  print("If you want to stop adding people, enter X.")
-  my_guest = add_guest.title()
-  guest_list = []
+guests = [] # Creates an empty list
+
+def invitees_name(): # We don't want to type the same code three times. So we create a function
+  print("Add someone you want to invite:")
+  my_guest = input() # Gets the input
+  return my_guest.title() # Capitalize it correctly
+
+guests.append(invitees_name())  # Use the function as many times you want
+guests.append(invitees_name())
+guests.append(invitees_name())
+
+for i in guests: # Prints the value of each index 
+  print(f"I would like to invite you, {i} to my party this Saturday!")
+
+
