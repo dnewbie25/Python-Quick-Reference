25c8373069fdb2c47f1272e3c98b41abec0d82d4
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2020-05-29 23:02:00 -0500

    Delete 3_Guest_List.py

--- Example Exercises/3_Guest_List.py
+++
@@ -1,6 +0,0 @@
-print("Add the people you want to invite. Remember they must have first name and last name.")
-
-def guests(add_guest):
-  print("If you want to stop adding people, enter X.")
-  my_guest = add_guest.title()
-  guest_list = []
