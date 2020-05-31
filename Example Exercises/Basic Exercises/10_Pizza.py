ae4a7bef4b27d6c680173cc96c1e7119585e7146
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2020-05-31 12:00:29 -0500

    Create 10_Pizza.py

---
+++ Example Exercises/Basic Exercises/10_Pizza.py
@@ -0,0 +1,17 @@
+pizzas = ['Napolitan', 'Margaritta', 'Hawaian']
+
+for pizza in pizzas: # Loops through all the pizzas and prints the values at each index
+  print("One of my favorite pizzas is " + pizza)
+
+# Prints 3 lines separarated by the \n command
+print("I love the meat\nThe cheese is wonderful\nAnd the ingredients are so fresh!\I just love pizza\n\n")
+
+animals = ['lion', 'zebra', 'tiger']
+
+for animal in animals: # Loops through all the animals and prints a specific message for each one
+  if animal == 'lion':
+    print("Lion is the king of beats")
+  elif animal == 'zebra':
+    print("They all look the same")
+  else:
+    print("The tiger is just amazing")
