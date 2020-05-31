31bc34d3ec100d774f4b39f7e32e3d3d4240c51a
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2020-05-31 17:29:41 -0500

    Create 13_Tuples.py

---
+++ Example Exercises/Basic Exercises/13_Tuples.py
@@ -0,0 +1,15 @@
+buffet = ('cannolis', 'pesto', 'salmon', 'caviar', 'gold ice cream')
+
+# For loop
+
+for food in buffet:
+  print("The menu includes: " + food)
+
+# Modify an item
+
+# buffet[3] = 'meat' -->> This one causes an error, as it should be
+
+buffet = ('cannolis', 'pesto', 'salmon', 'regular fish', 'vanilla ice cream')
+
+for food in buffet:
+  print("The menu includes: " + food)
