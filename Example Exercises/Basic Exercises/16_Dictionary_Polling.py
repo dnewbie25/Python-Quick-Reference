02d4250b99530c3a2a235ea6470a2c4bb87b1e60
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2020-06-02 22:00:12 -0500

    Create 16_Dictionary_Polling.py

---
+++ Example Exercises/Basic Exercises/16_Dictionary_Polling.py
@@ -0,0 +1,30 @@
+# Create a dictionary with the favorite programming languages of certain people
+
+favorite_languages = {
+ 'jen': 'python',
+ 'sarah': 'c',
+ 'edward': 'ruby',
+ 'phil': 'python',
+ }
+
+# Create  alist of people missing for saying their favorite language
+
+people_missing = ['max', 'tim', 'sarah', 'rachel', 'thomas', 'phil', 'phillip']
+
+# Create a for loop for every name in the list
+new_dictionary = {}
+# Here you want to use the 'in' or 'not in' statements
+
+for names in favorite_languages.keys():
+  for missings in people_missing:
+    if
+
+      
+
+
+  
+  
+      
+
+      
+print(favorite_languages)
