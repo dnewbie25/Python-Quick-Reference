d511343ed68e0116e0625ddccf518408cba238f3
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2020-06-03 11:50:25 -0500

    Update 16_Dictionary_Polling.py

--- Example Exercises/Basic Exercises/16_Dictionary_Polling.py
+++ Example Exercises/Basic Exercises/16_Dictionary_Polling.py
@@ -12,12 +12,14 @@ favorite_languages = {
 people_missing = ['max', 'tim', 'sarah', 'rachel', 'thomas', 'phil', 'phillip']
 
 # Create a for loop for every name in the list
-new_dictionary = {}
-# Here you want to use the 'in' or 'not in' statements
 
-for names in favorite_languages.keys():
-  for missings in people_missing:
-    if
+# Here you want to use the 'in' or 'not in' statements to loop through the list of people missing and compare the name with the keys of the idctionary
+
+for names in people_missing:
+  if names in favorite_languages.keys():
+    print(f"\nThank you for responding, {names}")
+  else:
+    print(f"\nPlease fill up the poll, {names}")
 
       
 
