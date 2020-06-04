9ae62df091c95ab29a4ec0b0dc6bdbf19acd8426
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2020-06-04 16:08:57 -0500

    Update C2_Rock_Paper_Scissors.py

--- Automate The Boring Stuff Projects/C2_Rock_Paper_Scissors.py
+++ Automate The Boring Stuff Projects/C2_Rock_Paper_Scissors.py
@@ -6,23 +6,25 @@ def computer():
   choice = random.randint(0,2)
 
   if choice == 0: # Rock
-    return 'r'
+    return 'rock'
   elif choice == 1: # Paper
-    return 'p'
+    return 'paper'
   else:   # Scissors
-    return 's'
+    return 'scissors'
 
 # Player inputs her choice
 
 def player():
   choice = input("\nEnter rock (r), paper (p), scissors (s) or 'q' to quit: ")
 
-  if choice == 'q' or 'Q':
+  if choice == 'q' or choice == 'Q':
     sys.exit()
-  elif choice == 'r' or 'R':
+  elif choice == 'r' or choice == 'R':
     return 'rock'
-  elif choice == 'p' or 'P':
+  elif choice == 'p' or choice == 'P':
     return 'paper'
+  elif choice == 's' or choice == 'S':
+    return 'scissors'
   else:
     print("Wrong value. Try again") # If the user enters an invalid value, this will send False
     return False
@@ -30,25 +32,73 @@ def player():
 # Determine if the player wins, losses or ties
 
 def match(user, machine):
-  if user == 'rock': # Player chooses ROCK
-    if machine == 'r':
+
+  if user == False: # If user enters an invalid character
+    return 'failed'
+  elif user == 'rock': # Player chooses ROCK
+    if machine == 'rock':
       return 'tie'
-    elif machine == 'p':
+    elif machine == 'paper':
       return 'loose'
     else:
       return 'win'
   elif user == 'paper': # Player chooses PAPER
-    if machine == 'r':
+    if machine == 'rock':
       return 'win'
-    elif machine == 'p':
+    elif machine == 'paper':
       return 'tie'
     else:
       return 'loose'
   else:   # Player chooses SCISSORS
-    if machine == 'r':
+    if machine == 'rock':
       return 'loose'
-    elif machine == 'p':
+    elif machine == 'paper':
       return 'win'
     else:
       return 'tie'
 
+# Gameplay, 5 rounds
+
+attempts = 0
+wins = 0
+losses = 0
+ties = 0
+
+while attempts < 5:
+  user_input = player()
+  computer_input = computer()
+
+  play_round = match(user_input, computer_input)
+
+  if play_round == 'failed':
+    print("Invalid input. Loose 1 attempt")
+    attempts+=1
+  elif play_round == 'win':
+    print(f"Player choose: {user_input.title()}")
+    print(f"Computer choose: {computer_input.title()}")
+    print("\nYou win this one!")
+    wins+=1
+    attempts+=1
+  elif play_round == 'loose':
+    print(f"Player choose: {user_input.title()}")
+    print(f"Computer choose: {computer_input.title()}")
+    print("You lost this one")
+    losses+=1
+    attempts+=1
+  else:
+    print(f"Player choose: {user_input.title()}")
+    print(f"Computer choose: {computer_input.title()}")
+    print("It's a tie")
+    ties+=1
+    attempts+=1
+
+# Determines the winner
+
+print(f"\nFinal Score: Wins {wins}, Losses {losses} and Ties {ties}")
+
+if wins > losses:
+  print("\nCongrats!! You won")
+elif wins < losses:
+  print("\nYou LOST everything")
+else:
+  print("It's a tie -_-")
