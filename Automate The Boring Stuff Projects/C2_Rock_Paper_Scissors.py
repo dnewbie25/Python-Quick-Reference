e45fc83c55c52dad044d3c3089dda3cf5d36ced2
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2020-06-04 15:47:14 -0500

    Create C2_Rock_Paper_Scissors.py

---
+++ Automate The Boring Stuff Projects/C2_Rock_Paper_Scissors.py
@@ -0,0 +1,54 @@
+import random, sys
+
+# Create the computer's choice
+
+def computer():
+  choice = random.randint(0,2)
+
+  if choice == 0: # Rock
+    return 'r'
+  elif choice == 1: # Paper
+    return 'p'
+  else:   # Scissors
+    return 's'
+
+# Player inputs her choice
+
+def player():
+  choice = input("\nEnter rock (r), paper (p), scissors (s) or 'q' to quit: ")
+
+  if choice == 'q' or 'Q':
+    sys.exit()
+  elif choice == 'r' or 'R':
+    return 'rock'
+  elif choice == 'p' or 'P':
+    return 'paper'
+  else:
+    print("Wrong value. Try again") # If the user enters an invalid value, this will send False
+    return False
+
+# Create 1 round
+
+def match(user, machine):
+  if user == 'rock': # Player chooses ROCK
+    if machine == 'r':
+      return 'tie'
+    elif machine == 'p':
+      return 'loose'
+    else:
+      return 'win'
+  elif user == 'paper': # Player chooses PAPER
+    if machine == 'r':
+      return 'win'
+    elif machine == 'p':
+      return 'tie'
+    else:
+      return 'loose'
+  else:   # Player chooses SCISSORS
+    if machine == 'r':
+      return 'loose'
+    elif machine == 'p':
+      return 'win'
+    else:
+      return 'tie'
+
