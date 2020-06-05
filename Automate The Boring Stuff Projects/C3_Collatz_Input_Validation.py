dd77e29cdf1d72e276eed7a2ee9df71119331b75
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2020-06-04 21:26:44 -0500

    Create C3_Collatz_Input_Validation.py

---
+++ Automate The Boring Stuff Projects/C3_Collatz_Input_Validation.py
@@ -0,0 +1,33 @@
+# Creates the collatz function
+
+def collatz(number):
+  if number % 2 == 0: # If even
+    return number // 2
+  else: # If odd
+    return number * 3 + 1
+
+# User enters a number
+
+def user_input(): 
+  try:  # The user has 1 one try to not crash the program
+    number = int(input("Enter an integer number: "))
+    return number
+  except ValueError: # If the input is not a number the try will be given to user. 
+    print("You entered an invalid input. Try again")
+    number = int(input("Enter an integer number: "))
+    return number
+
+# Loops until it returns 1
+
+def collatz_loop():
+  value = user_input() # assigns the user's input to a variable
+  result = collatz(value) # creates an initial collatz with the user's input
+  while result >= 1:
+    if result != 1: # While result is different from 1 it continues looping
+      print(result)
+      result = collatz(result)
+    else: # If result is equal to 1 it stops
+      print(result)
+      break
+
+collatz_loop()
