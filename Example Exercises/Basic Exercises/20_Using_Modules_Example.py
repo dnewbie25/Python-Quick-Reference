b7584f14f4bc268652efac0c4b767cedfa5997e0
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2020-06-06 12:03:03 -0500

    Modules exmaple

---
+++ Example Exercises/Basic Exercises/20_Using_Modules_Example.py
@@ -0,0 +1,7 @@
+import Module_Example_19
+
+Module_Example_19.hello_human('Rachel')
+
+print("\n\n")
+
+Module_Example_19.arithmetics(15, 12)
---
+++ Example Exercises/Basic Exercises/Module_Example_19.py
@@ -0,0 +1,12 @@
+# Modules' name can't start with a number, so that's why the name of this file is different
+
+def hello_human(name):
+  print(f"Hello {name}")
+
+def arithmetics(number1, number2):
+  print(f"Sum: {number1 + number2}")
+  print(f"Substraction: {number1 - number2}")
+  print(f"Multiplication: {number1 * number2}")
+  print(f"Division: {number1 / number2}")
+  print(f"Floor Division: {number1 // number2}")
+  print(f"Modulo: {number1 % number2}")
