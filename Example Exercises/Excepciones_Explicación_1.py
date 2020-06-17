4dda3ca93ecd2192e29967bb188cbfe784264616
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2020-06-16 21:47:11 -0500

    excepts

--- Example Exercises/Excepciones_Explicación_1.py
+++ Example Exercises/Excepciones_Explicación_1.py
@@ -9,11 +9,28 @@ def substract(num1, num2):
 def multiply(num1, num2):
   return num1 * num2
 
-def divide(num1, num2):
-  return num1 / num2
+'''def divide(num1, num2):''' # Si el denominador es cero esto va a causar un crash
+'''return num1 / num2'''
 
-choice_1 = int(input("Enter number 1: "))
-choice_2 = int(input("Enter number 2: "))
+# Para evitar eso usamos un try - except
+
+def divide(num1, num2):
+  try:
+    return num1 / num2
+  except ZeroDivisionError:
+    print("No se puede dividir entre cero")
+    return "Operación incorrecta"
+
+# También si el usuario ingresa letras en lugar de número va a dar un ValueError. Esto se puede corregir así:
+
+while True:
+  try:
+
+    choice_1 = int(input("Enter number 1: "))
+    choice_2 = int(input("Enter number 2: "))
+    break  # Si se ingresan los valores correctos, o sea números, el programa va a salir del loop
+  except ValueError:
+    print("Los valores ingresados son incorrectos. Intenta con números enteros.") # Si no, se ejecutará el except y volverá a ejecutar el while loop
 
 operation = input("Enter the operation you want to perform (sum, substract, multiply, divide): ")
 
---
+++ Example Exercises/Excepciones_Explicación_2.py
@@ -0,0 +1,28 @@
+def divide():
+
+  try:
+    num1 = float(input("Introduce el primero número: ")) 
+    num2 = float(input("Introduce el segundo número: "))
+
+    print("La división es " + str(num1 / num2))
+
+  '''Los except se pueden declarar consecutivamente en caso de que puedan ocurrir muchos errores, como aquí donde puede dar error si se ingresan letras en vez de números o si se ingresa cero como denominador'''
+
+  except ValueError: # Si no es un número
+
+    print("El valor introducido es incorrecto")
+
+  except ZeroDivisionError: # Si el denominador es cero
+    print("No se puede dividir entre cero")
+
+  '''
+  También se puede poner except solito para que capture todas las excepciones, pero no ayuda al usuario a saber qué pasó pues no le darías ningún mensaje específico
+
+  except:
+    print("El valor es incorrecto o el denominador es cero")
+
+  '''
+
+  print("Cálculo finalizado")
+
+divide()
