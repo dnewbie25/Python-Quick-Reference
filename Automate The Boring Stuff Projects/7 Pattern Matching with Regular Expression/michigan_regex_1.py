36beed06e83683e21c4e2ba15ab6be04e051f967
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2020-06-30 10:27:01 -0500

    Update michigan_regex_1.py

--- Automate The Boring Stuff Projects/7 Pattern Matching with Regular Expression/michigan_regex_1.py
+++ Automate The Boring Stuff Projects/7 Pattern Matching with Regular Expression/michigan_regex_1.py
@@ -4,9 +4,7 @@ num_list = list()
 for line in filename:
     matches = re.findall('[0-9]+', line)
     num_list.append(matches)
-    '''for item in matches:
-      integers = int(item)
-      num_list.append(integers)'''
+    
 
 unwrapped_list = []
 
@@ -22,11 +20,4 @@ for num in unwrapped_list:
 
 print("The total is: ",total)
 
-#print(num_list)
 print(len(unwrapped_list))
-'''total = 0
-for num in num_list:
-    total += num
-'''
-#rint(total)
-#print(len(num_list))
