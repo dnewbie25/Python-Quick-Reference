49ac76a66590e3359d30e24acfdf649494b7d5d9
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2020-06-30 10:27:48 -0500

    Update michigan_regex_1.py

--- Automate The Boring Stuff Projects/7 Pattern Matching with Regular Expression/michigan_regex_1.py
+++ Automate The Boring Stuff Projects/7 Pattern Matching with Regular Expression/michigan_regex_1.py
@@ -3,18 +3,18 @@ filename = open('Files Samples/regex_sum_723743.txt')
 num_list = list()
 for line in filename:
     matches = re.findall('[0-9]+', line)
-    num_list.append(matches)
+    num_list.append(matches) # return a list  full of sub lists
     
 
 unwrapped_list = []
 
-for item1 in num_list:
+for item1 in num_list: # unwraps the lists and creates just one list with no sublists
   for item2 in item1:
     unwrapped_list.append(item2)
 
 total = 0
 
-for num in unwrapped_list:
+for num in unwrapped_list: # sum the values in the new unwrapped list
   num = int(num)
   total += num
 
