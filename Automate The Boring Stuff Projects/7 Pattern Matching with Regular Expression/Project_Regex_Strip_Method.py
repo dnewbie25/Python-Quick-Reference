3b88cc1baea7d31492f141553c495b505cb4b58d
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2020-07-26 18:12:16 -0500

    Update Project_Regex_Strip_Method.py

--- Automate The Boring Stuff Projects/7 Pattern Matching with Regular Expression/Project_Regex_Strip_Method.py
+++ Automate The Boring Stuff Projects/7 Pattern Matching with Regular Expression/Project_Regex_Strip_Method.py
@@ -1,2 +1,23 @@
 """Write a function that does the same as the strip( ) method
+The strip method removes whitespaces at the start and end of a string
 """
+
+import re
+
+text_to_strip = input("Enter your text here:\n")
+
+def myOwnStrip(text):
+
+  replace = ''
+
+  startWhitespaceRegex = re.compile(r'^\s+') # matches starting whitespaces
+
+  endWhitespaceRegex = re.compile(r'\s+$') # matches whitespaces at the end
+
+  no_start_whitespace_string = startWhitespaceRegex.sub(replace, text) # replace starting whitespaces with no characters
+
+  no_end_whitespace_string = endWhitespaceRegex.sub(replace, no_start_whitespace_string) # replace ending whitespaces with no characters
+
+  return no_end_whitespace_string
+
+print(myOwnStrip(text_to_strip))
