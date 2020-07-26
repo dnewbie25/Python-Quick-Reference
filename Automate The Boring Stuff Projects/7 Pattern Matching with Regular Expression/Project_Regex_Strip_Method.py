cce3bf31a7ceb53c7cda99ade268b490da4736ca
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2020-07-26 18:18:33 -0500

    Update Project_Regex_Strip_Method.py

--- Automate The Boring Stuff Projects/7 Pattern Matching with Regular Expression/Project_Regex_Strip_Method.py
+++ Automate The Boring Stuff Projects/7 Pattern Matching with Regular Expression/Project_Regex_Strip_Method.py
@@ -6,18 +6,37 @@ import re
 
 text_to_strip = input("Enter your text here:\n")
 
-def myOwnStrip(text):
+def myOwnStrip(text, character = ''):
 
-  replace = ''
+  if character == '':
+    replace = ''
 
-  startWhitespaceRegex = re.compile(r'^\s+') # matches starting whitespaces
+    startWhitespaceRegex = re.compile(r'^\s+') # matches starting whitespaces
 
-  endWhitespaceRegex = re.compile(r'\s+$') # matches whitespaces at the end
+    endWhitespaceRegex = re.compile(r'\s+$') # matches whitespaces at the end
 
-  no_start_whitespace_string = startWhitespaceRegex.sub(replace, text) # replace starting whitespaces with no characters
+    no_start_whitespace_string = startWhitespaceRegex.sub(replace, text) # replace starting whitespaces with no characters
 
-  no_end_whitespace_string = endWhitespaceRegex.sub(replace, no_start_whitespace_string) # replace ending whitespaces with no characters
+    no_end_whitespace_string = endWhitespaceRegex.sub(replace, no_start_whitespace_string) # replace ending whitespaces with no characters
 
-  return no_end_whitespace_string
+    return no_end_whitespace_string
+  else:
 
-print(myOwnStrip(text_to_strip))
+    replace = ''
+
+    startWhitespaceRegex = re.compile(r'^\s+') # matches starting whitespaces
+
+    endWhitespaceRegex = re.compile(r'\s+$') # matches whitespaces at the end
+
+    no_start_whitespace_string = startWhitespaceRegex.sub(replace, text) # replace starting whitespaces with no characters
+
+    no_end_whitespace_string = endWhitespaceRegex.sub(replace, no_start_whitespace_string) # replace ending whitespaces with no characters
+
+    characterRegex = re.compile(character, re.IGNORECASE)
+
+    string_without_character = characterRegex.sub(replace, no_end_whitespace_string) # takes the no_end_whitespace_string, which is the string without starting nor ending whitespaces and removes the specified character
+
+    return string_without_character
+
+
+print(myOwnStrip(text_to_strip, 'o'))
