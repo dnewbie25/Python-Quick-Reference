425ac29cf4f47b3cb954e70e827dddb129f75170
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2020-07-28 16:30:38 -0500

    Update Project_Keep_People_Busy.py

--- Automate The Boring Stuff Projects/8 Input Validation/Project_Keep_People_Busy.py
+++ Automate The Boring Stuff Projects/8 Input Validation/Project_Keep_People_Busy.py
@@ -1,8 +1,8 @@
 import sys
 import pyinputplus as pyip 
 
-
-
+# Method one
+"""
 def keepBusyHours():
   response = pyip.inputYesNo("Do you want to know how to keep an idio busy for hourse?\n", caseSensitive=False, yesVal='yes')
 
@@ -14,7 +14,18 @@ def keepBusyHours():
     keepBusyHours()
 
 keepBusyHours()
-    
+""" 
 
+# Method two
+
+while True:
+  question = "Do you want to know how to keep in idiot busy?\n"
+  response = pyip.inputYesNo(question)
+
+  if response == 'yes':
+    continue
+  elif response == 'no':
+    print("Thank you for playing. Have a great day!")
+    break
 
 
